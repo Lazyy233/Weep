5adf1c1e323ff1eaefce2104ce7dde664c40bcc1
Lazyy233 <xpierce@hotmail.com>
2020-10-27 20:26:52 +0000

    Initial commit

---
+++ src/info/membercount.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "membercount",
+    category: "info",
+    description: "shows server information",
+    run: (client, message, args) => {
+
+        let Embed = new Discord.MessageEmbed()
+            .setColor(0xdfa914)
+            .setTitle("Member Count:")
+            .setColor(0x5126c7)
+            .setDescription(`${message.guild.memberCount}`)
+            .setFooter("Made by @Lazyy#7933")
+        message.channel.send(Embed);
+    }
+}
